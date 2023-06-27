2e050cb85c99ff35aa8079f541577d57c4207b54
Lee-J-Hwa <juoc00@naver.com>
2023-06-27 18:01:09 +0900

    portfolio80%

---
+++ index.js
@@ -0,0 +1,71 @@
+$('.project-list li')
+  .eq(0)
+  .on('mouseenter', () => {
+    $('.pro-1 img').stop().animate({ right: 100 }, 300);
+  });
+$('.project-list li')
+  .eq(0)
+  .on('mouseleave', () => {
+    $('.pro-1 img').stop().animate({ right: -1000 }, 300);
+  });
+$('.project-list li')
+  .eq(1)
+  .on('mouseenter', () => {
+    $('.pro-2 img').stop().animate({ right: 100 }, 300);
+  });
+$('.project-list li')
+  .eq(1)
+  .on('mouseleave', () => {
+    $('.pro-2 img').stop().animate({ right: -1000 }, 300);
+  });
+$('.project-list li')
+  .eq(2)
+  .on('mouseenter', () => {
+    $('.pro-3 img').stop().animate({ right: 100 }, 300);
+  });
+$('.project-list li')
+  .eq(2)
+  .on('mouseleave', () => {
+    $('.pro-3 img').stop().animate({ right: -1000 }, 300);
+  });
+
+$('.project-list li')
+  .eq(3)
+  .on('mouseenter', () => {
+    $('.pro-4 img').stop().animate({ right: 100 }, 300);
+  });
+$('.project-list li')
+  .eq(3)
+  .on('mouseleave', () => {
+    $('.pro-4 img').stop().animate({ right: -1000 }, 300);
+  });
+$('.project-list li')
+  .eq(4)
+  .on('mouseenter', () => {
+    $('.pro-5 img').stop().animate({ right: 100, bottom: -5 }, 300);
+  });
+$('.project-list li')
+  .eq(4)
+  .on('mouseleave', () => {
+    $('.pro-5 img').stop().animate({ right: -1000, bottom: 0 }, 300);
+  });
+$('.project-list li')
+  .eq(5)
+  .on('mouseenter', () => {
+    $('.pro-6 img').stop().animate({ right: 100, bottom: -5 }, 300);
+  });
+$('.project-list li')
+  .eq(5)
+  .on('mouseleave', () => {
+    $('.pro-6 img').stop().animate({ right: -1000, bottom: 0 }, 300);
+  });
+$('.project-list li')
+  .eq(6)
+  .on('mouseenter', () => {
+    $('.pro-7 img').stop().animate({ right: 100, bottom: -5 }, 300);
+  });
+$('.project-list li')
+  .eq(6)
+  .on('mouseleave', () => {
+    $('.pro-7 img').stop().animate({ right: -1000, bottom: 0 }, 300);
+  });
