58c45028e07b71bfe4105fda1984ce18b0b304ac
unknown <holicjudi@naver.com>
2023-07-28 16:43:39 +0900

    canon-sub

---
+++ project/subpage/canon-sub.js
@@ -0,0 +1,9 @@
+// home 화면 밖에서 menu 색 변경
+$(window).on('scroll',()=>{
+  const menu = $('.project').offset().top-100;
+   if ( scrollY > menu) {
+     $('header').addClass('color-change');
+   } else {
+     $('header').removeClass('color-change');
+   }
+ });
